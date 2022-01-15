bae461e00a1e23a8a11369ce900f350362af2122
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-15 14:03:58 -0500

    feat: add "GAMConfig" base model to hold gam config variables

--- gam_core/gamconfig.py
+++ gam_core/gamconfig.py
@@ -1,1 +1,13 @@
 """Manage IO to a GAM config file."""
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+
+
+class GAMConfig(BaseModel):
+    """GAM config variables."""
+
+    cache_dir: str = Field(
+        default=lambda: Path.home().joinpath("/.cache/gam").as_posix()
+    )
+    repositories: list[str] = Field(default=lambda: [])
