e499d00f67858e87018e30e4443712c8fee6a259
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-15 12:33:30 -0500

    test: add unit test for creating new gam project

--- tests/test_gam_core.py
+++ tests/test_gam_core.py
@@ -1,9 +1,18 @@
 """GAM Core unit tests."""
+import os
 import unittest
+from pathlib import Path
+
+from gam_core import config
 
 
 class ConfigTest(unittest.TestCase):
-    pass
+    def test_new(self) -> None:
+        name = "test_config"
+        root_dir = Path(os.getcwd()) / name
+        config.new(root_dir, name)
+        gamproject = config.load(root_dir)
+        self.assertEqual(gamproject.name, name)
 
 
 class BuildTest(unittest.TestCase):
