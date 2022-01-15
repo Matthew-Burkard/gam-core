61849a68b4c607896ee29691d1fc346e82fa99f7
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-15 14:41:10 -0500

    feat: save and load gam config files

--- gam_core/gamconfig.py
+++ gam_core/gamconfig.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 
 from pydantic import BaseModel, Field
+from tomlkit import dumps, parse
 
 
 class GAMConfig(BaseModel):
@@ -11,3 +12,16 @@ class GAMConfig(BaseModel):
         default=lambda: Path.home().joinpath("/.cache/gam").as_posix()
     )
     repositories: list[str] = Field(default=lambda: [])
+
+
+def load(path: str | Path | None) -> GAMConfig:
+    """Get a GAM config object from a file."""
+    path = Path(path) if path else Path.home().joinpath("./config/gam/")
+    return GAMConfig(**parse(path.read_text())["gam"])
+
+
+def save(path: str | Path | None, config: GAMConfig | None) -> None:
+    """Save a GAM config file."""
+    path = Path(path) if path else Path.home().joinpath("./config/gam/")
+    config = config or GAMConfig()
+    path.write_text(dumps({"gam": config.dict(exclude_unset=True)}))
