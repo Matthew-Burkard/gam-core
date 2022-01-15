8100d0d73f40b73f781cedefec031b0dcccc70f1
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-15 16:31:50 -0500

    refactor: gamproject is public

--- gam_core/gamproject.py
+++ gam_core/gamproject.py
@@ -1,12 +1,14 @@
 """Provides GAMProject properties definition."""
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class GAMProject(BaseModel):
     """Dataclass for GAM project properties."""
+
     name: str
     version: str
     godot_version: str
+    packages: list[str] = Field(default=lambda: [])
     description: str | None
     repository: str | None
     homepage: str | None
