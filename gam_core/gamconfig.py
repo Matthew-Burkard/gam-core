8d2053064b6c101a7e395de9b2ec586860ba8e95
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-29 23:07:27 -0500

    feat: GAMConfig path is now a property
    Changing "GAMConfig.path" will load the config file at the new path.

--- gam_core/gamconfig.py
+++ gam_core/gamconfig.py
@@ -9,13 +9,23 @@ class GAMConfig:
 
     __instance__ = None
 
-    def __init__(self, path: Path) -> None:
+    def __init__(self) -> None:
         if GAMConfig.__instance__ is not None:
             raise AssertionError("Instance of singleton GAMConfig already exists.")
-        self.path: Path = path
-        gam = _load(self.path)
-        self.cache_dir: Path = gam.get("cache_dir") or (Path.home() / ".cache/gam")
-        self.repositories: list[str] = gam.get("repositories") or []
+        self._path: Path = Path.home().joinpath("./config/gam/config.toml")
+        self.cache_dir: Path = (Path.home() / ".cache/gam")
+        self.repositories: list[str] = []
+        self.load()
+
+    @property
+    def path(self) -> Path:
+        """Path to "config.toml" file."""
+        return self._path
+
+    @path.setter
+    def path(self, p: Path) -> None:
+        self._path = p
+        self.load()
 
     @staticmethod
     def get_instance() -> "GAMConfig":
@@ -25,7 +35,7 @@ class GAMConfig:
         """
         if GAMConfig.__instance__ is not None:
             return GAMConfig.__instance__
-        return GAMConfig(Path.home().joinpath("./config/gam/config.toml"))
+        return GAMConfig()
 
     def save(self) -> None:
         """Save a GAM config file."""
@@ -35,8 +45,10 @@ class GAMConfig:
                 "repositories": self.repositories,
             }
         }
-        self.path.write_text(dumps(gam_dict))
+        self._path.write_text(dumps(gam_dict))
 
-
-def _load(path: Path | str) -> dict:
-    return parse(path.read_text())["gam"]
+    def load(self) -> None:
+        """Load values from config file at path,"""
+        gam = parse(self._path.read_text())["gam"]
+        self.cache_dir = gam.get("cache_dir")
+        self.repositories = gam.get("repositories")
