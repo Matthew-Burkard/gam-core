976a57e150d9a932a5f65f38120717405a35299b
Matthew Burkard <matthewjburkard@gmail.com>
2022-02-03 23:29:45 -0500

    feat: add function to gather dependencies

--- gam_core/version.py
+++ gam_core/version.py
@@ -1,4 +1,16 @@
 """Check version compatability."""
+from gam_core.requirement import Requirement
+
+
+def gather_dependencies(requirements: list[Requirement]) -> list[Requirement]:
+    """Gather all dependencies for the given requirements."""
+    for requirement in requirements:
+        for dependency in requirement.project_details.dependencies:
+            if any(matches(dependency, r.requirement_string) for r in requirements):
+                continue
+            requirements.append(Requirement(dependency))
+            return gather_dependencies(requirements)
+    return requirements
 
 
 def matches(version: str, requirement: str) -> bool:
