81b9bf07dc7764afc6c3b9d6846b506272659aa7
Matthew Burkard <matthewjburkard@gmail.com>
2022-02-06 13:43:06 -0500

    test: ensure config integrity is preserved when changing cache_dir
    Add test for updating config location.

--- tests/test_gam_config.py
+++ tests/test_gam_config.py
@@ -13,6 +13,10 @@ class GAMConfigTests(unittest.TestCase):
         config = GAMConfig.get_instance(gam_config)
         config.save()
         self.assertEqual(raw_text, gam_config.read_text())
+        cache_dir = Path().cwd() / "gam_cache"
+        config.cache_dir = cache_dir
+        config.save()
+        self.assertEqual(raw_text, gam_config.read_text())
 
     def test_default_config_location(self) -> None:
         GAMConfig.__instance__ = None
@@ -25,13 +29,24 @@ class GAMConfigTests(unittest.TestCase):
     def test_custom_config_location(self) -> None:
         GAMConfig.__instance__ = None
         gam_config = Path().cwd() / "gam_config/config.toml"
-        cache_dir = Path().cwd() / "/gam_cache"
+        cache_dir = Path().cwd() / "gam_cache"
         config = GAMConfig.get_instance(gam_config)
         config.cache_dir = cache_dir
         self.assertEqual(gam_config, config.path)
         config.save()
         self.assertTrue(gam_config.exists())
-        self.assertEqual(cache_dir, config.cache_dir)
+        self.assertEqual(cache_dir.resolve(), config.cache_dir.resolve())
+
+    def test_updating_config_location(self) -> None:
+        GAMConfig.__instance__ = None
+        config = GAMConfig.get_instance()
+        gam_config = Path().cwd() / "gam_config/config.toml"
+        config.path = gam_config
+        cache_dir = Path().cwd() / "gam_cache"
+        self.assertEqual(cache_dir.resolve(), config.cache_dir.resolve())
+
+    def test_new_config(self) -> None:
+        pass
 
     def test_singleton(self) -> None:
         GAMConfig.__instance__ = None
