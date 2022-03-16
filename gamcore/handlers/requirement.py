8e62c716ce041af59be7a5de35d5c588d51bbc3e
Matthew Burkard <matthewjburkard@gmail.com>
2022-03-16 12:07:18 -0400

    fix: use proper filepath getting project details

--- gamcore/handlers/requirement.py
+++ gamcore/handlers/requirement.py
@@ -25,6 +25,12 @@ class RequirementHandler:
     """Represents a GAM project requirement."""
 
     def __init__(self, requirement_string: str):
+        # TODO Split on "@"
+        self._name: str | None = None
+        self._version_rule: str | None = None
+        self._filepath: str | None = None
+        if "@" in requirement_string:
+            self._name, self._version_rule = requirement_string.split("@")
         self.requirement_string = requirement_string
         if self._is_filepath():
             self.source = RequirementSourceType.FILE
@@ -41,6 +47,7 @@ class RequirementHandler:
     @property
     def project_details(self) -> GAMProject:
         """The project details for this requirement."""
+        # TODO When lock files is thing, search there first.
         if self._project_details is None:
             match self.source:
                 case RequirementSourceType.FILE:
@@ -58,7 +65,7 @@ class RequirementHandler:
         tmp_dir = GAMConfig.get_instance().tmp_dir
         tmp_pkg_dir = tmp_dir.joinpath(uid)
         tmp_pkg_dir.mkdir(exist_ok=True)
-        _unzip_tar(self.requirement_string, tmp_pkg_dir)
+        _unzip_tar(self._filepath, tmp_pkg_dir)
         # Search content of tmp_pkg_dir to get extracted directory name.
         unpacked_pkg_dir = None
         for path, directories, files in os.walk(tmp_pkg_dir):
@@ -81,10 +88,20 @@ class RequirementHandler:
         pass  # TODO
 
     def _is_filepath(self) -> bool:
-        return (
-            Path(self.requirement_string).is_file()
-            or Path.cwd().joinpath(self.requirement_string).is_file()
-        )
+        if self._version_rule is not None:
+            if Path(self._version_rule).is_file():
+                self._filepath = self._version_rule
+                return True
+            if Path.cwd().joinpath(self._version_rule).is_file():
+                self._filepath = Path.cwd().joinpath(self._version_rule)
+                return True
+        if Path(self.requirement_string).is_file():
+            self._filepath = Path(self.requirement_string)
+            return True
+        if Path.cwd().joinpath(self.requirement_string).is_file():
+            self._filepath = Path.cwd().joinpath(self.requirement_string)
+            return True
+        return False
 
     def _is_git(self) -> bool:
         pass  # TODO
